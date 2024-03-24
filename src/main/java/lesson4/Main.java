b4c4a47568fb807947c273898400b05b1a2f9c49
Alexey Neroda <neroda.all@mail.ru>
2024-03-24 15:06:30 +0300

    hw 4 done

---
+++ src/main/java/lesson4/Main.java
@@ -0,0 +1,79 @@
+package lesson4;
+
+import lesson1.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Configuration configuration = new Configuration().configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlserver://localhost\\RESTO;" +
+                    "encrypt=false;trustServerCertificate=false;databaseName=Test;user=java;password=java");) {
+                // Student student = new Student(3L, "Vova", "Neroda", 18L);
+                // AddStudent(sessionFactory, student);
+                // System.out.println(findById(sessionFactory,1L));
+                // UpdateStudent(sessionFactory,1L,"Alexei");
+                //  RemoveStudent(sessionFactory,1L);
+                SelectOldStudents(sessionFactory);
+            }
+        }
+
+    }
+
+    public static void AddStudent(SessionFactory sessionFactory, Student student) throws SQLException {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(student);
+            tx.commit();
+        }
+    }
+
+    public static Student findById(SessionFactory sessionFactory, long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.find(Student.class, id);
+        }
+    }
+
+    public static void UpdateStudent(SessionFactory sessionFactory, Long id, String newName) throws SQLException {
+        try (Session session = sessionFactory.openSession()) {
+            Student student = findById(sessionFactory, id);
+            student.setFirstName(newName);
+            Transaction tx = session.beginTransaction();
+            session.merge(student);
+            tx.commit();
+        }
+    }
+
+    public static void RemoveStudent(SessionFactory sessionFactory, Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Student student = findById(sessionFactory, id);
+            Transaction tx = session.beginTransaction();
+            session.remove(student);
+            tx.commit();
+        }
+    }
+
+    public static void SelectOldStudents (SessionFactory sessionFactory){
+        try (Session session = sessionFactory.openSession()) {
+
+            Query<Student> query = session.createQuery("select s from Student s where age > 20",Student.class);
+            List<Student> resultList = query.getResultList();
+            System.out.println(resultList);
+
+        }
+    }
+
+}
---
+++ src/main/java/lesson4/Student.java
@@ -0,0 +1,64 @@
+package lesson4;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String secondName;
+
+    @Column
+    private Long age;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
